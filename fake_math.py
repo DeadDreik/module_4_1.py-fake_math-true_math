57bdcb990efd0550f15790e617be87e95f26eb18
Shamil <65324697+DeadDreik@users.noreply.github.com>
2024-08-19 18:29:22 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,8 @@
+def divide(first, second):
+    if second == 0:
+        print("Ошибка")
+    else:
+        print(first / second)
+divide(2,0)
+if __name__ == '__main__':
+    main()
---
+++ module4_1.py
@@ -0,0 +1,6 @@
+from fake_math import divide as f_m
+from true_math import divide as t_m
+f_m(2,0)
+f_m(8,2)
+t_m(2,0)
+t_m(6,2)
