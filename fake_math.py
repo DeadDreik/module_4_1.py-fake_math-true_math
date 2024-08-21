cd257005d287fed1881cb249874a8ff2d40df312
Shamil <65324697+DeadDreik@users.noreply.github.com>
2024-08-22 01:41:57 +0300

    Add files via upload

--- fake_math.py
+++ fake_math.py
@@ -1,8 +1,5 @@
 def divide(first, second):
     if second == 0:
-        print("Ошибка")
+        return 'Ошибка'
     else:
-        print(first / second)
-divide(2,0)
-if __name__ == '__main__':
-    main()
+        return first / second
--- true_math.py
+++ true_math.py
@@ -1,7 +1,7 @@
 from math import inf
+
 def divide(first, second):
     if second == 0:
-        print(inf)
+        return inf
     else:
-        print(first / second)
-divide(2,0)
+        return first / second
